0f08d0a1f59fc9f1c1238b3763deb23b5be736c4
derrick12 <derrickwaweru@gmail.com>
2018-08-06 17:34:56 +0300

    add multiplication function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-var add = function(number1, number2) {
-  return number1 + number2;
+var multiplication = function(number1, number2) {
+  return number1 * number2;
 };
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
+var result = multiplication(number1, number2);
 alert(result);
